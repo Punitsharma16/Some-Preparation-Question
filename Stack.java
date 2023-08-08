c34577e286ed9bfc10ffe879946f392694746dbd
Punitsharma16 <punitsharma9034908543@gmail.com>
2023-08-08 16:10:40 +0530

    theoretical

--- Stack.java
+++ Stack.java
@@ -27,5 +27,6 @@ public class Stack {
            when we put a new books on them, that is push in stack.     ----->      |  C.S     |  Books
            when we remove a book from the top, that is pop in stack.               |  math    |
                                                                                    |__________|
-
+  Note --->   for a better experience use stack with Array list or linked list.
+              time complexity is O(1) of the push, pop and peek normally.
  */
