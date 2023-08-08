f227a9c7f4cae2473ac05a6f2db1c21161b72269
Punitsharma16 <punitsharma9034908543@gmail.com>
2023-08-08 15:03:04 +0530

    theoretical

---
+++ Difference_Between_the_Arrays_And_Linked_List.java
@@ -0,0 +1,27 @@
+public class Difference_Between_the_Arrays_And_Linked_List {
+}
+/*
+          Arrays                                                   Linked List
+
+  1. Array is a group of data elements                1. Linked list is a group of entities(Node)
+       of similar data type.                              Node = data and address.
+
+  2. Data stores in contiguous                        2. Data is not stored in contiguous
+      memory location.                                   memory location ( stored randomly).
+
+  3. Memory size is Fixed.                            3. Linked list is dynamic in size.
+
+  4. We can access elements easily                    4. In linked list elements accessing needs to
+      and faster.                                         the traversal of whole linked list.
+
+  5. In array deletion and insertion                  5. Here insertion and deletion operation
+      takes more time as compare to                       is faster.
+      linked list.
+
+  6. Memory is assigned at compile time.              6. Memory is assigned at run time.
+
+  7. Elements of arrays are not dependent                7. Elements of linked list is dependent
+     on each other.                                       on each other.
+
+
+ */
